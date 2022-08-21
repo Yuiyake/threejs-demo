aec1fe67c70a20a53e96150a010626cb43448c31
yuiyake <1306824899@qq.com>
2022-08-22 00:25:49 +0800

    第二章的04

---
+++ src/main/main2-01.js
@@ -0,0 +1,47 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+// 目标： 物体缩放和旋转
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+// 设置相机位置
+camera.position.set(0,0,10);
+scene.add(camera);
+
+// 添加物体
+// 创建几何体
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array( [
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    -1.0, 1.0, 1.0,
+    -1.0, -1.0, 1.0,
+])
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+const material = new THREE.MeshBasicMaterial({color: 0xffff00});
+const mesh = new THREE.Mesh(geometry, material);
+
+// 将几何体添加进场景
+scene.add(mesh);
+// 初始化渲染器
+const render = new THREE.WebGLRenderer();
+render.setSize(window.innerWidth, window.innerHeight);
+// 将webgl渲染的canvas添加到body上
+document.body.appendChild(render.domElement);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, render.domElement);
+// 轨道控制
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper);
+
+function renderFn() {
+    // 使用渲染器，通过相机将场景渲染
+    render.render(scene, camera);
+    // 渲染下一帧的时候调用renderFn
+    requestAnimationFrame(renderFn);
+}
+renderFn();
